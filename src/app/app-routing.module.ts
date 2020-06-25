1bee3dfbb2425fcb82a904d63599c1047cda4dc2
Christian <chsanleo@gmail.com>
2020-06-25 21:50:00 +0200

    Routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "", component: MoviesComponent },
+  { path: "login", component: LoginComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "signup", component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
