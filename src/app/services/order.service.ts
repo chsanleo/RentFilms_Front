15b8d50b4f7f2bd00e653f080925c8c66895d875
Christian <chsanleo@gmail.com>
2020-06-26 12:28:21 +0200

    Schema of all methods

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  apiUrl = 'http://localhost:3000/order';
+
+  constructor(private httpClient: HttpClient) { }
+
+  addOrder(): Observable<any> {
+    //router.post('/', OrderController.addOrder);
+    return;
+  }
+
+  getOrder(): Observable<any> {
+    //router.get('/:id', OrderController.getOrder);
+    return;
+  }
+
+  getAllOrder(): Observable<any> {
+    //router.get('/', OrderController.getAllOrder);
+    return;
+  }
+
+  updateOrder(): Observable<any> {
+    //router.put('/', OrderController.updateOrder);
+    return;
+  }
+
+  deleteOrder(): Observable<any> {
+    //router.delete('/:id', OrderController.deleteOrder);
+    return;
+  }
+
 }
