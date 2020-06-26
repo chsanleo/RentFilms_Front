f991a41f3f81708cc784014f3d3eafdba4f737d7
Christian <chsanleo@gmail.com>
2020-06-26 11:31:31 +0200

    Schema of all methods

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  apiUrl = 'http://localhost:3000/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  signIn():Observable<any>{
+    return this.httpClient.get(this.apiUrl + 'main/trending')
+  }
+
+  logIn():Observable<any>{
+    return ;
+  }
+  logOut():Observable<any>{
+    return;
+  }
+  getUser():Observable<any>{
+    return;
+  }
+  updateUserInfo():Observable<any>{
+    return;
+  }
+  deleteUser():Observable<any>{
+    return;
+  }
+
 }
