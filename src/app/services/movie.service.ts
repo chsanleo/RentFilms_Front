caa4347cd9d8aface21763119856536034b4dc68
Christian <chsanleo@gmail.com>
2020-06-28 13:16:31 +0200

    Create methods in MovieService

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,36 +1,66 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Movie } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  apiUrl = 'http://localhost:3000/';
+  private apiUrl = 'http://localhost:3000/';
+  private page = 0;
+  private movieDetail: Movie;
+  private moviesByTitle: Movie[];
 
   constructor(private httpClient: HttpClient) { }
 
+  //GETTERS
+  getPage() { return this.page; }
+  getMovie() { return this.movieDetail; }
+  getMoviesByTitle() { return this.moviesByTitle; }
+
+  //SETTERS
+  setPage(page: number) {
+    this.page = page;
+  }
+
+  //METHODS
   getTrendingMovies(): Observable<any> {
+    
     return this.httpClient.get(this.apiUrl + 'main/trending');
-    //añadir comportamiento para 
-    //router.get('/trending/:id', MovieController.getTredingMovies); (pages)
+    // router.get('/trending/:id', MovieController.getTredingMovies);
 
   }
 
-  getAllGendersMovies(): Observable<any> {
-    return this.httpClient.get(this.apiUrl + '/genders');
+  getMovieDetail(id: number) {
+    this.httpClient.get<Movie>(this.apiUrl + '/movies/' + id)
+      .subscribe(data => {
+        this.movieDetail.poster_path = data.poster_path,
+          this.movieDetail.idIMDB = data.idIMDB,
+          this.movieDetail.original_language = data.original_language,
+          this.movieDetail.original_title = data.original_title,
+          this.movieDetail.genre_id = data.genre_id,
+          this.movieDetail.title = data.title,
+          this.movieDetail.overview = data.overview,
+          this.movieDetail.release_date = data.release_date
+      });
   }
 
-  getMovieById():Observable<any>{
-    //router.get('/:id', MovieController.getMovie);
-    return;
+  getGender(id: number) {
+    //router.get('/genders/:id', MovieController.getGenders);
+
   }
-  
-  searchMovieByTitle():Observable<any>{
-    //router.post('title', 
-    return;
+
+  getMovieByTitle(title: string) : Observable<any> {
+    let body = { title: title };
+
+    //recoger varios en lista
+
+    return this.httpClient.post<any>(this.apiUrl + 'movies/title', body);
+      /*.subscribe({
+        next: data => console.log("todoOK"),
+        error: error => console.log(error)
+      });*/
   }
-  
-//router.get('/genders/:id', MovieController.getGenders);
 }
