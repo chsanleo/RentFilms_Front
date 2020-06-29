208e79e68bfb3052727048ad2bcdad4bf9d3158e
Christian <chsanleo@gmail.com>
2020-06-30 00:12:15 +0200

    Create Movie detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,6 +11,7 @@ import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
   { path: "", component: MoviesComponent },
+  { path: "movieDetail", component: MovieDetailComponent },
   { path: "login", component: LoginComponent },
   { path: "profile", component: ProfileComponent },
   { path: "signup", component: SignupComponent },
--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -9,12 +9,12 @@ import { Movie } from 'src/app/models/movie.model';
 })
 export class MovieDetailComponent implements OnInit {
 
-  private movieDetail:Movie;
+  movieDetail:Movie;
 
   constructor(private MovieService : MovieService) { }
 
   ngOnInit(): void {
-    this.MovieService.getMovieDetail(4565)
+    this.MovieService.getMovieDetail(4562)
     .subscribe(
       res => this.updateDataMovieDetail(res), 
       error => console.log(error)
@@ -22,13 +22,14 @@ export class MovieDetailComponent implements OnInit {
   }
 
   private updateDataMovieDetail(movie:Movie){
-    this.movieDetail.poster_path = movie.poster_path,
+    this.movieDetail = movie;
+   /* this.movieDetail.poster_path = movie.poster_path,
     this.movieDetail.idIMDB = movie.idIMDB,
     this.movieDetail.original_language = movie.original_language,
     this.movieDetail.original_title = movie.original_title,
     this.movieDetail.genre_id = movie.genre_id,
     this.movieDetail.title = movie.title,
     this.movieDetail.overview = movie.overview,
-    this.movieDetail.release_date = movie.release_date
+    this.movieDetail.release_date = movie.release_date*/
   }
 }
