766a78afd36cfee6b5495fb0ffadafcd55bdf7e1
Christian <chsanleo@gmail.com>
2020-07-02 18:00:35 +0200

    Fix addres error
    And rename Interfaces

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,34 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Order } from '../models/iorder.model';
+import { IOrder } from '../models/iorder.model';
 import { Injectable } from '@angular/core';
+import { IOrderShow } from '../modelsShow/orderShow.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  private apiUrl = 'http://localhost:3000/order';
+  private apiUrl = 'http://localhost:3000/orders';
 
   constructor(private httpClient: HttpClient) { }
 
-  addOrder(order: Order): Observable<any> {
+  addOrder(order: IOrder): Observable<any> {
     return this.httpClient.post(this.apiUrl + '/', order);
     //router.post('/', OrderController.addOrder);
   }
 
-  getOrder(id: number): Observable<Order> {
-    return this.httpClient.get<Order>(this.apiUrl + '/' + id);
+  getOrder(id: number): Observable<IOrder> {
+    return this.httpClient.get<IOrder>(this.apiUrl + '/' + id);
     //router.get('/:id', OrderController.getOrder);
-    
-  }
-  getOrdersByUser(userId:number) : Observable<Order> {
-    return this.httpClient.get<Order>(this.apiUrl+'/user/'+userId);
-  }
 
-  getAllOrder(): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(this.apiUrl + '/');
+  }
+  getOrdersByUser(): Observable<IOrderShow[]> {
+    return this.httpClient.get<IOrderShow[]>(this.apiUrl + '/user');
+  }///
+
+  getAllOrder(): Observable<IOrder[]> {
+    return this.httpClient.get<IOrder[]>(this.apiUrl + '/');
     //router.get('/', OrderController.getAllOrder);
   }
 }
