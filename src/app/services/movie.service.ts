bac96815ac77b462bf4b4a7593170cfb81574f0f
Christian <chsanleo@gmail.com>
2020-07-02 18:03:03 +0200

    Create method to return all orders of a User
     & Rename variables with I of interfaces

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Movie } from '../models/movie.model';
+import { IMovie } from '../models/imovie.model';
+import { IOrderShow } from '../modelsShow/orderShow.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ export class MovieService {
 
   private apiUrl = 'http://localhost:3000/';
   private page = 0;
-  private movieDetail: Movie;
-  private moviesByTitle: Movie[];
+  private movieDetail: IMovie;
+  private moviesByTitle: IMovie[];
 
   constructor(private httpClient: HttpClient) { }
 
@@ -27,30 +28,37 @@ export class MovieService {
 
   //METHODS
   getTrendingMovies(): Observable<any> {
-    
+
     return this.httpClient.get(this.apiUrl + 'main/trending');
     // router.get('/trending/:id', MovieController.getTredingMovies);
 
   }
 
-  getMovieDetail(id: number) : Observable<Movie>{
-    return this.httpClient.get<Movie>(this.apiUrl + '/movies/' + id);
+  getMovieDetail(id: number): Observable<IMovie> {
+    return this.httpClient.get<IMovie>(this.apiUrl + '/movies/' + id);
+  }
+
+  getSomeMovies(movies: number[]) {
+    return this.httpClient.post<IOrderShow>(this.apiUrl + '/movies/movies', movies);
   }
 
   getGender(id: number) {
     //router.get('/genders/:id', MovieController.getGenders);
-
   }
 
-  getMovieByTitle(title: string) : Observable<any> {
+  getMovieByTitle(title: string): Observable<any> {
     let body = { title: title };
 
     //recoger varios en lista
 
     return this.httpClient.post<any>(this.apiUrl + 'movies/title', body);
-      /*.subscribe({
-        next: data => console.log("todoOK"),
-        error: error => console.log(error)
-      });*/
+    /*.subscribe({
+      next: data => console.log("todoOK"),
+      error: error => console.log(error)
+    });*/
+  }
+
+  getMovieById(id: string) {
+    return this.httpClient.get<IMovie>(this.apiUrl + '/movie/' + id);
   }
 }
