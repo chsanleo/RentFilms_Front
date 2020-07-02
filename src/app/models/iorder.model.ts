8680be409b7d4d42e08e50b5036654b502801fdd
Christian <chsanleo@gmail.com>
2020-07-02 10:46:42 +0200

    Fix the deleted of OrderService & Model

---
+++ src/app/models/iorder.model.ts
@@ -0,0 +1,5 @@
+export interface Order{
+    MovieId:string,
+    UserID:number,
+    returnDate:Date
+}
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,3 +1,7 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Order } from '../models/iorder.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +9,26 @@ import { Injectable } from '@angular/core';
 })
 export class OrderService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/order';
+
+  constructor(private httpClient: HttpClient) { }
+
+  addOrder(order: Order): Observable<any> {
+    return this.httpClient.post(this.apiUrl + '/', order);
+    //router.post('/', OrderController.addOrder);
+  }
+
+  getOrder(id: number): Observable<Order> {
+    return this.httpClient.get<Order>(this.apiUrl + '/' + id);
+    //router.get('/:id', OrderController.getOrder);
+    
+  }
+  getOrdersByUser(userId:number) : Observable<Order> {
+    return this.httpClient.get<Order>(this.apiUrl+'/user/'+userId);
+  }
+
+  getAllOrder(): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(this.apiUrl + '/');
+    //router.get('/', OrderController.getAllOrder);
+  }
 }
