bfb5f394ddbf64ed4a8cc5771b3d613e7058c11a
Christian <chsanleo@gmail.com>
2020-07-02 10:47:54 +0200

    Preparation logic to show history Order in profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service'
 import { IUser } from 'src/app/models/iuser.model';
+import { IOrder } from 'src/app/models/iorder.model';
+
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +13,13 @@ import { IUser } from 'src/app/models/iuser.model';
 export class ProfileComponent implements OnInit {
 
   user: IUser;
+  order: IOrder;
 
-  constructor(private UserService: UserService) { }
+  constructor(private UserService: UserService, private OrderService: OrderService) { }
 
   ngOnInit(): void {
     this.user = this.UserService.getUser();
+    this.order = this.OrderService.getOrdersByUser(this.user.id);
   }
 
 }
