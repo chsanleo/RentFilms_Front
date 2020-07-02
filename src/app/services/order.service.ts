b38463ff67aa4746d110a3a99d27883a4ab284b6
Christian <chsanleo@gmail.com>
2020-07-02 09:47:19 +0200

    Create link with back service Order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,26 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Order } from '../models/iorder.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  apiUrl = 'http://localhost:3000/order';
+  private apiUrl = 'http://localhost:3000/order';
 
   constructor(private httpClient: HttpClient) { }
 
-  addOrder(): Observable<any> {
+  addOrder(order: Order): Observable<any> {
+    this.httpClient.post(this.apiUrl + '/', order);
     //router.post('/', OrderController.addOrder);
     return;
   }
 
-  getOrder(): Observable<any> {
+  getOrder(id: number): Observable<any> {
+    this.httpClient.get<Order>(this.apiUrl + '/' + id);
     //router.get('/:id', OrderController.getOrder);
     return;
   }
 
   getAllOrder(): Observable<any> {
+    this.httpClient.get<Order[]>(this.apiUrl + '/');
     //router.get('/', OrderController.getAllOrder);
     return;
   }
