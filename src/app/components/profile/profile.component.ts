b247fbdd903fd4c9fe0aa930232276a239e264be
Christian <chsanleo@gmail.com>
2020-07-05 01:53:10 +0200

    Profile Add orders first attemp

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service'
-import { IUser } from 'src/app/models/iuser.model';
-import { IOrder } from 'src/app/models/iorder.model';
-
+import { UserService } from '../../services/user.service';
 import { OrderService } from 'src/app/services/order.service';
+import { MovieService } from 'src/app/services/movie.service';
+import { IUser } from 'src/app/models/iuser.model';
+
+
+import { IOrderShow } from 'src/app/modelsShow/orderShow.model';
 
 @Component({
   selector: 'app-profile',
@@ -13,13 +15,24 @@ import { OrderService } from 'src/app/services/order.service';
 export class ProfileComponent implements OnInit {
 
   user: IUser;
-  order: IOrder;
+  orders: IOrderShow[];
 
-  constructor(private UserService: UserService, private OrderService: OrderService) { }
+  constructor(
+    private UserService: UserService, 
+    private OrderService: OrderService, 
+    private MovieService: MovieService) { }
 
   ngOnInit(): void {
     this.user = this.UserService.getUser();
-    this.order = this.OrderService.getOrdersByUser(this.user.id);
+    this.OrderService.getOrdersByUser()
+      .subscribe(
+        res => this.UpdateHistoryOrder(res),
+        error => console.error(error),
+      );
   }
 
+  private UpdateHistoryOrder(orderDB: IOrderShow[]) {
+    this.orders.concat(orderDB);
+    //this.MovieService.getMovieById(orderDB._id)
+  }
 }
