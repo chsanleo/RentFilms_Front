d6a5169bb857cb36b6c49794a132f28900d45a3c
Christian <chsanleo@gmail.com>
2020-07-05 16:50:25 +0200

    Create logic Logic Detail

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
 import { IMovie } from 'src/app/models/imovie.model';
 import { IMovieExt } from 'src/app/models/imovieEXT.model';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -13,7 +13,7 @@ export class MoviesComponent implements OnInit {
 
   listOfMovies: IMovieExt[];
 
-  constructor(private MovieService: MovieService) { }
+  constructor(private MovieService: MovieService, private router: Router) { }
 
   ngOnInit(): void {
     this.MovieService.getTrendingMovies()
@@ -23,5 +23,26 @@ export class MoviesComponent implements OnInit {
         () => console.log(this.listOfMovies)
       )
   }
+  goMovieDetail(movie: IMovieExt) {
+    this.MovieService.getMovieDetail(movie.id)
+      .subscribe(
+        res => {
+          this.UpdateMovieDetailVar(res),
+            this.moveToDetail()
+        },
+        error => {
+          alert("Please try again in some minuts."),
+            console.log(error)
+        },
+      );
+  }
 
+  private UpdateMovieDetailVar(movie: IMovie) {
+    this.MovieService.setMovieDetailVar(movie);
+  }
+  private moveToDetail() {
+    setTimeout(() => {
+      this.router.navigate(['/movieDetail']);
+    }, 1500);
+  }
 }
